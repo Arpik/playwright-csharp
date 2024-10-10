a9f74515fe994e3977fbe6413b123406ba3d239a
Arpik Sargsyan <arpiksargsyan@gmail.com>
2024-10-10 16:14:16 +0500

    Add simple example of filling the form

---
+++ UnitTest1.cs
@@ -0,0 +1,42 @@
+using Microsoft.Playwright;
+
+namespace PlaywrightProject;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        // Create playwright instance
+        using var playwright = await Playwright.CreateAsync();
+
+        // Create browser instance
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false
+        });
+
+        // Create page instance
+        var page = await browser.NewPageAsync();
+
+        // Go to particular page
+        await page.GotoAsync("https://demoqa.com/");
+
+        // simple example of filling the form
+        await page.ClickAsync("text = Elements");
+        await page.ClickAsync("text = Text Box");
+        await page.FillAsync("#userName", "important user");
+        await page.FillAsync("#userEmail", "importantemail@email.com");
+        await page.FillAsync("#currentAddress", "Important Street 77/88BHL");
+        await page.FillAsync("#permanentAddress", "This one is permanent address");
+        await page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "filledform.jpg"
+        });
+    }
+}
