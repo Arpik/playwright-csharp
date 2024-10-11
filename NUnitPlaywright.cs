555a40ece6ab69c907cc831405546a236cd032ac
Arpik Sargsyan <arpiksargsyan@gmail.com>
2024-10-11 23:56:04 +0500

    Switched the file names

--- NUnitPlaywright.cs
+++ NUnitPlaywright.cs
@@ -1,38 +1,27 @@
-﻿using Microsoft.Playwright;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
 
 namespace PlaywrightProject;
 
-public class Tests
+public class NUnitPlaywright : PageTest
 {
     [SetUp]
-    public void Setup()
+    public async Task Setup()
     {
+        // Go to particular page
+        await Page.GotoAsync("https://demoqa.com/");
     }
 
     [Test]
     public async Task Test1()
     {
-        // Create playwright instance
-        using var playwright = await Playwright.CreateAsync();
-
-        // Create browser instance
-        var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-        {
-            Headless = false
-        });
-
-        // Create page instance
-        var page = await browser.NewPageAsync();
-
-        // Go to particular page
-        await page.GotoAsync("https://demoqa.com/");
-        await page.ClickAsync("text = Elements");
-        await page.ClickAsync("text = Text Box");
-        await page.FillAsync("#userName", "important user");
-        await page.FillAsync("#userEmail", "importantemail@email.com");
-        await page.FillAsync("#currentAddress", "Important Street 77/88BHL");
-        await page.FillAsync("#permanentAddress", "This one is permanent address");
-        await page.ScreenshotAsync(new PageScreenshotOptions
+        await Page.ClickAsync("text = Elements");
+        await Page.ClickAsync("text = Text Box");
+        await Page.FillAsync("#userName", "important user");
+        await Page.FillAsync("#userEmail", "importantemail@email.com");
+        await Page.FillAsync("#currentAddress", "Important Street 77/88BHL");
+        await Page.FillAsync("#permanentAddress", "This one is permanent address");
+        await Page.ScreenshotAsync(new PageScreenshotOptions
         {
             Path = "filledform.jpg"
         });
---
+++ UnitTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Playwright;
+
+namespace PlaywrightProject;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        // Create playwright instance
+        using var playwright = await Playwright.CreateAsync();
+
+        // Create browser instance
+        var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false
+        });
+
+        // Create page instance
+        var page = await browser.NewPageAsync();
+
+        // Go to particular page
+        await page.GotoAsync("https://demoqa.com/");
+        await page.ClickAsync("text = Elements");
+        await page.ClickAsync("text = Text Box");
+        await page.FillAsync("#userName", "important user");
+        await page.FillAsync("#userEmail", "importantemail@email.com");
+        await page.FillAsync("#currentAddress", "Important Street 77/88BHL");
+        await page.FillAsync("#permanentAddress", "This one is permanent address");
+        await page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "filledform.jpg"
+        });
+    }
+}
