17a182770e8eca564306a6b27967e069bb5c6ba2
Arpik Sargsyan <arpiksargsyan@gmail.com>
2024-10-11 23:45:32 +0500

    Add PageTest to reduce implementation

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,8 +1,9 @@
 using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
 
 namespace PlaywrightProject;
 
-public class Tests
+public class NUnitPlaywright : PageTest
 {
     [SetUp]
     public void Setup()
@@ -12,32 +13,17 @@ public class Tests
     [Test]
     public async Task Test1()
     {
-        // Create playwright instance
-        using var playwright = await Playwright.CreateAsync();
-
-        // Create browser instance
-        var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-        {
-            Headless = false
-        });
-
-        // Create page instance
-        var page = await browser.NewPageAsync();
-
         // Go to particular page
-        await page.GotoAsync("https://demoqa.com/");
-        await page.ClickAsync("text = Elements");
-        await page.ClickAsync("text = Text Box");
-        await page.FillAsync("#userName", "important user");
-        await page.FillAsync("#userEmail", "importantemail@email.com");
-        await page.FillAsync("#currentAddress", "Important Street 77/88BHL");
-        await page.FillAsync("#permanentAddress", "This one is permanent address");
-        await page.ScreenshotAsync(new PageScreenshotOptions
+        await Page.GotoAsync("https://demoqa.com/");
+        await Page.ClickAsync("text = Elements");
+        await Page.ClickAsync("text = Text Box");
+        await Page.FillAsync("#userName", "important user");
+        await Page.FillAsync("#userEmail", "importantemail@email.com");
+        await Page.FillAsync("#currentAddress", "Important Street 77/88BHL");
+        await Page.FillAsync("#permanentAddress", "This one is permanent address");
+        await Page.ScreenshotAsync(new PageScreenshotOptions
         {
             Path = "filledform.jpg"
         });
-
-        // Perform a specific action
-        await page.ClickAsync("text = Elements");
     }
 }
